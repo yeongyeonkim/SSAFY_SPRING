1f2f551e8f0e10c6986fb44c1c35f732e9b053e8
KimYeongYeon <dudtbd111@naver.com>
2019-10-31 09:36:41 +0900

    SpringMVC 업데이트

---
+++ SpringMVC/src/main/java/com/mybatis/service/BoardService.java
@@ -0,0 +1,19 @@
+package com.mybatis.service;
+
+import java.util.List;
+
+import com.mybatis.vo.Board;
+
+//client(BoardApp.java)를 위한 인터페이스
+public interface BoardService {
+	public List<Board> selectAll();
+	public Board selectOne(String num);
+	public void insert(Board c);
+	public void delete(String num);
+	public void update(Board b);
+	public List<Board> findByTitle(String title);
+	public List<Board> findByName(String name);
+	
+	public List<Board> findByTitle2(String title);
+	public void delete2(String[] nums);
+}
